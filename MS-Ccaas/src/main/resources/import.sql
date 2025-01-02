6160b51a6ffbefc4993b2d43cf797ace1b536eab
David Hernández <114569835+daviidhache@users.noreply.github.com>
2025-01-02 13:21:39 +0100

    Delete MS-Ccaas directory

--- MS-Ccaas/src/main/resources/import.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO comunidades (nombre,plazas_cubiertas) values("Extremadura",0);
-INSERT INTO comunidades (nombre,plazas_cubiertas) values("Andalucia",0);
-INSERT INTO comunidades (nombre,plazas_cubiertas) values("Madrid",0);
-INSERT INTO comunidades (nombre,plazas_cubiertas) values("Galicia",0);
